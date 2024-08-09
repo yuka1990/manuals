e326b77c2b064cbd78ccfb94d0e567721f835bc4
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-08-09 07:49:12 +0000

    アソシエーション設定

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,2 +1,5 @@
 class Department < ApplicationRecord
+  
+  has_many :users, dependent: :destroy
+  
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,7 @@
 class Request < ApplicationRecord
+  
+  belongs_to :user
+  belongs_to :category
+
+
 end
